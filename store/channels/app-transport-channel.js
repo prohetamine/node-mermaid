4ba4faa559312665e2d42656b25430dffe13f5cb
Stas Prohetamine <prohetamine@gmail.com>
2023-01-13 04:27:15 +0300

    update store

--- store/channels/app-transport-channel.js
+++ store/channels/app-transport-channel.js
@@ -1,23 +1,23 @@
 module.exports = (io, openWindowCallback) => {
   io.on('connection', socket => {
     if (socket.handshake.query.platform === 'app-transport-channel') {
-      socket.join(`${socket.handshake.query.repo}/${socket.handshake.query.app}`)
+      socket.join(`${socket.handshake.query.repository}/${socket.handshake.query.app}`)
 
       socket.on('writeData', data => {
       socket
-        .to(`${socket.handshake.query.repo}/${socket.handshake.query.app}`)
+        .to(`${socket.handshake.query.repository}/${socket.handshake.query.app}`)
         .emit('writeData', data)
       })
 
       socket.on('readData', data => {
       socket
-        .to(`${socket.handshake.query.repo}/${socket.handshake.query.app}`)
+        .to(`${socket.handshake.query.repository}/${socket.handshake.query.app}`)
         .emit('readData', data)
       })
 
       socket.on('bluetoothProvider', data => {
       socket
-        .to(`${socket.handshake.query.repo}/${socket.handshake.query.app}`)
+        .to(`${socket.handshake.query.repository}/${socket.handshake.query.app}`)
         .emit('bluetoothProvider', data)
       })
 
