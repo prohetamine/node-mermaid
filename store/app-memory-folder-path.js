a103c0f8cbda396bf20e21b6f8b15daab4737af8
Stas Prohetamine <prohetamine@gmail.com>
2023-01-30 16:08:40 +0300

    fix

--- store/app-memory-folder-path.js
+++ store/app-memory-folder-path.js
@@ -1,7 +1,5 @@
 const { io } = require('socket.io-client')
     , path = require('path')
 
-module.exports = () => {
-  const [repository, app] = process.argv.slice(2)
-  return path.join(__dirname, '../../../../../../memory', repository, app)
-}
+const [repository, app] = process.argv.slice(2)
+module.exports = path.join(__dirname, '../../../../../../memory', repository, app)
